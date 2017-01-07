bdd282a2316b804e492fad53fae80290ca1916e7
Xin Zheng(EASC-2020 IT-OS Delivery-CD1) <zhengxin5@asiainfo.com>
2017-01-07 15:20:40 +0800

    new file:   .gitignore

--- iphone-launchImage-generator.sh
+++ iphone-launchImage-generator.sh
@@ -4,11 +4,11 @@ then
 	exit 1
 fi
 
-path=launchImage
+path=launchImageoutput
 mkdir $path
 echo "Generating iPhone launch image."
-
-sips -s format png -z 640 960 $1 --out $path/launch@2x.png
-sips -s format png -z 750 1334 $1 --out $path/launch@retina4.7.png
-sips -s format png -z 640 1136 $1 --out $path/launch@retina4.png
-sips -s format png -z 1242 2208 $1 --out $path/launch@retina5.5.png
+echo $1
+sips -z 960 960 $1 --out $path/launch@2x.png
+sips -z 750 1334 $1 --out $path/launch@retina4.7.png
+sips -z 640 1136 $1 --out $path/launch@retina4.png
+sips -z 1242 2208 $1 --out $path/launch@retina5.5.png
