74b75728f49a5fb3ad492bb90dc02ed73f26656d
ZX <bumaociyuan@users.noreply.github.com>
2019-05-24 17:53:07 +0800

    Update iphone-icon-generator.sh

--- iphone-icon-generator.sh
+++ iphone-icon-generator.sh
@@ -20,7 +20,7 @@ sips -s format png -z 60 60 $1 --out $path/Icon-60.png
 sips -s format png -z 120 120 $1 --out $path/Icon-60@2x.png
 sips -s format png -z 180 180 $1 --out $path/Icon-60@3x.png
 
-sips -s format png -z 57 57 $1 --out $path/Icon.png
-sips -s format png -z 114 114 $1 --out $path/Icon@2x.png
+# sips -s format png -z 57 57 $1 --out $path/Icon.png
+# sips -s format png -z 114 114 $1 --out $path/Icon@2x.png
 
 sips -s format png -z 1024 1024 $1 --out $path/iTunesArtwork
